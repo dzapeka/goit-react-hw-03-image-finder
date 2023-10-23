d6989635672e7383515e68e1942a11a7ca447934
dzapeka <douson@gmail.com>
2023-10-24 01:18:59 +0200

    misc fix

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -3,13 +3,13 @@ import styles from './Modal.module.css';
 
 export default class Modal extends Component {
   componentDidMount() {
-    window.addEventListener('keydown', this.handleKeyDown);
     document.body.style.overflow = 'hidden';
+    window.addEventListener('keydown', this.handleKeyDown);
   }
 
   componentWillUnmount() {
-    window.removeEventListener('keydown', this.handleKeyDown);
     document.body.style.overflow = 'auto';
+    window.removeEventListener('keydown', this.handleKeyDown);
   }
 
   handleKeyDown = event => {
